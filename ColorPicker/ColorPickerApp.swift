bfc5be342c81d3d03a19174da656b10af1f97e40
Satej <Satej>
2023-01-02 07:50:02 +0530

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Satej Sahu on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
